89dd42315d462b287548d69c1721419e52081014
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-10 18:29:14 +0200

    branch with master

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 var span = $("span");
-span.each(function(index, element) {
-  $("span:even").css('color', 'red');
-  });
+$("span:even").each(function(index, element) {
+  $(element).css('color', 'red');
+});
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
   var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
   $(element).append(button) 
-}); 
-$("button").click(function(){
+});
+$("button").click(function() {
   alert($(this).attr("data-tmp"));
-}); 
+});
+
